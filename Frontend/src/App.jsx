533b2448ca4cbcfb094e905bda8686203084abef
Friedbocovo <friedbocovo03@gmail.com>
2025-09-26 19:20:19 +0200

    initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+function App() {
+
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center justify-center bg-gray-600">
+        <h1 className="text-8xl text-gray-200 ">Welcome Team 👌</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
